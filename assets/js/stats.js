941eb0670d7df420ad364ac75e5933ce4716a3ca
mellamojori <mellamojori@gmail.com>
2023-09-11 14:06:41 -0300

    Add responsive property to table and improve code

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,9 +1,9 @@
 let events = [];
 idContainer = "statsContainer"
 errorMessage = 'Oh no! We were unable to retrieve events stats at this time <i class="bi bi-emoji-dizzy"></i> <br>Please try again later';
-tbAll = document.getElementById("allStats");
-tbUpcoming = document.getElementById("upcomingStats");
-tbpast = document.getElementById("pastStats");
+const tbodyAll = document.getElementById("allStats");
+const tbodyUpcoming = document.getElementById("upcomingStats");
+const tbodypast = document.getElementById("pastStats");
 
 fetchData(getEventsStats);
 
@@ -11,20 +11,12 @@ function getEventsStats(currentDate, allEvents) {
     let tbodyHTML = "";
     const upcomingEvents = allEvents.filter(event => event.date > currentDate);
     const pastEvents = allEvents.filter(event => event.date <= currentDate);
-    let assistancePercentageByEvent = getAssistancePercentageByEvent(pastEvents);
-    let highestAssistanceEvents = assistancePercentageByEvent.slice(0, 3);
+    
+    let highestAssistanceEvents = pastEvents.sort((a, b) => (b.assistance * 100) / b.capacity - (a.assistance * 100) / a.capacity).slice(0, 3);
 
-    let lowestAssistanceEvents = assistancePercentageByEvent.sort((eventA, eventB) => {
-        if (eventB.percentage > eventA.percentage) {
-            return -1;
-        }
-    }).slice(0, 3);
+    let lowestAssistanceEvents = pastEvents.sort((a, b) => (a.assistance * 100) / a.capacity - (b.assistance * 100) / b.capacity).slice(0, 3);
 
-    let largerCapacityEvents = allEvents.sort((eventA, eventB) => {
-        if (eventB.capacity < eventA.capacity) {
-            return -1;
-        }
-    }).slice(0, 3)
+    let largerCapacityEvents = allEvents.sort((a, b) => b.capacity - a.capacity).slice(0, 3);
 
     for (let i = 0; i < 3; i++) {
         tbodyHTML += `<tr>
@@ -33,58 +25,43 @@ function getEventsStats(currentDate, allEvents) {
             <td class="bg-transparent text-info">${largerCapacityEvents[i].name}</td>
         </tr>`;
     }
-    tbAll.innerHTML = tbodyHTML;
+    tbodyAll.innerHTML = tbodyHTML;
 
     tbodyHTML = "";
     let upcomingEventsCategories = getUnrepiteCategories (upcomingEvents);
     upcomingEventsCategories.forEach(category => {
         let filteredUpcomingEvents = upcomingEvents.filter(event => event.category === category);
-        let revenueByCategory = getRevenueByCategory(filteredUpcomingEvents, true);
-        let percentageEstimatedAssistance = getPercentageEstimatedAssistance(filteredUpcomingEvents, true);
+        let proyectedRevenueByCategory = getRevenueByCategory(filteredUpcomingEvents, true);
+        let proyectedPercentageAssistance = getPercentageAssistance(filteredUpcomingEvents, true);
         tbodyHTML += `<tr>
             <td class="bg-transparent text-info">${category}</td>
-            <td class="bg-transparent text-info">$${revenueByCategory}</td>
-            <td class="bg-transparent text-info">${percentageEstimatedAssistance}%</td>
+            <td class="bg-transparent text-info">$${proyectedRevenueByCategory}</td>
+            <td class="bg-transparent text-info">${proyectedPercentageAssistance}%</td>
         </tr>`;
     });
     
-    tbUpcoming.innerHTML = tbodyHTML;
+    tbodyUpcoming.innerHTML = tbodyHTML;
     
     tbodyHTML = "";
     let pastEventsCategories = getUnrepiteCategories (pastEvents);
     pastEventsCategories.forEach(category => {
         let filteredPastEvents = pastEvents.filter(event => event.category === category);
         let revenueByCategory = getRevenueByCategory(filteredPastEvents, false);
-        let percentageEstimatedAssistance = getPercentageEstimatedAssistance(filteredPastEvents, false);
+        let percentageAssistance = getPercentageAssistance(filteredPastEvents, false);
         tbodyHTML += `<tr>
             <td class="bg-transparent text-info">${category}</td>
             <td class="bg-transparent text-info">$${revenueByCategory}</td>
-            <td class="bg-transparent text-info">${percentageEstimatedAssistance}%</td>
+            <td class="bg-transparent text-info">${percentageAssistance}%</td>
         </tr>`;
     });
 
-    tbpast.innerHTML = tbodyHTML;
+    tbodypast.innerHTML = tbodyHTML;
 }
 
-function getAssistancePercentageByEvent(pastEvents) {
-    const assistancePercentageByEvent = [];
-    pastEvents.forEach(event => {
-        let eventAssistancePercentage = (event.assistance / event.capacity) * 100;
-        let eventPercentage = { name: event.name, percentage: eventAssistancePercentage.toFixed(1) }
-        assistancePercentageByEvent.push(eventPercentage);
-    });
-
-    return assistancePercentageByEvent.sort((eventA, eventB) => {
-        if (eventB.percentage < eventA.percentage) {
-            return -1;
-        }
-    });
-}
-
-function getRevenueByCategory(events, isUpcoming){
+function getRevenueByCategory(events, isProyected){
     let revenueByCategory = 0;
     events.forEach(event => {
-        if(isUpcoming){
+        if(isProyected){
             revenueByCategory += event.price * event.estimate;
         }else{
             revenueByCategory += event.price * event.assistance;
@@ -93,12 +70,12 @@ function getRevenueByCategory(events, isUpcoming){
     return revenueByCategory;
 }   
 
-function getPercentageEstimatedAssistance(events, isUpcoming){
+function getPercentageAssistance(events, isProyected){
     let totalCapacity = 0;
     let totalAssistance = 0;
     events.forEach(event => {
         totalCapacity += event.capacity;
-        if(isUpcoming){
+        if(isProyected){
             totalAssistance += event.estimate;
         }else{
             totalAssistance += event.assistance;    
